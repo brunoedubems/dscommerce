50661ece57415177a51457ab143f9429e5ada5b0
brunoedubems <bemds1@aluno.ifnmg.edu.br>
2025-05-15 19:15:49 -0300

    Chave composta e orderItem

--- src/main/java/com/brunoedubems/dscommerce/entities/Order.java
+++ src/main/java/com/brunoedubems/dscommerce/entities/Order.java
@@ -1,6 +1,9 @@
 package com.brunoedubems.dscommerce.entities;
 
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
@@ -10,6 +13,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
@@ -32,12 +36,12 @@ public class Order {
   @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
   private Payment payment;
 
+  @OneToMany(mappedBy = "id.order")
+  private Set<OrderItem> items = new HashSet<>();
 
-  
   public Order() {
   }
 
-  
   public Order(Long id, Instant moment, OrderStatus status, User client, Payment payment) {
     this.id = id;
     this.moment = moment;
@@ -45,7 +49,6 @@ public class Order {
     this.client = client;
     this.payment = payment;
   }
-
 
   public Long getId() {
     return id;
@@ -79,5 +82,11 @@ public class Order {
     this.client = client;
   }
 
-  
+  public Set<OrderItem> getItems(){
+    return items;
+  }
+
+  public List<Product> getProducts(){
+    return items.stream().map(x-> x.getProduct()).toList();
+  }
 }
