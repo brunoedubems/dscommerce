d4f59c4f68ffda4b9d60a9aaef419e5054468c3d
brunoedubems <bemds1@aluno.ifnmg.edu.br>
2025-06-14 16:47:22 -0300

    post novo produto

--- src/main/java/com/brunoedubems/dscommerce/controllers/ProductController.java
+++ src/main/java/com/brunoedubems/dscommerce/controllers/ProductController.java
@@ -1,18 +1,26 @@
 package com.brunoedubems.dscommerce.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
 import com.brunoedubems.dscommerce.dto.ProductDTO;
 import com.brunoedubems.dscommerce.services.ProductService;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import java.util.List;
+
+import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
 
 @RestController
 @RequestMapping(value = "/products")
@@ -37,21 +45,51 @@ public class ProductController {
   //   Optional<Product> result = productRepository.findById(1L);
   //   Product product = result.get();
   //   return product.getName();
+  // // }
+  // @GetMapping(value = "/{id}")
+  // public ProductDTO findById(@PathVariable Long id) {
+  //  //ProductDTO dto = service.findById(id)
+  //  // return dto;
+  //  return service.findById(id);
   // }
   @GetMapping(value = "/{id}")
-  public ProductDTO findById(@PathVariable Long id) {
-   //ProductDTO dto = service.findById(id)
-   // return dto;
-   return service.findById(id);
+  public ResponseEntity<ProductDTO> findById(@PathVariable Long id) {
+
+   ProductDTO dto = service.findById(id);
+   return ResponseEntity.ok(dto); // customizando a resposta com o rensposeEntity
   }
-  // @GetMapping()
-  // public List<ProductDTO> findAll(Pageable pageable) {
-  //  return service.findAll(pageable);
-  // }
+
+
+
+
+    // @GetMapping()
+    // public List<ProductDTO> findAll(Pageable pageable) {
+    //  return service.findAll(pageable);
+    // }
+    // @GetMapping()
+    // public Page<ProductDTO> findAll(Pageable pageable) {
+    //  return service.findAll(pageable);
+    // }
   @GetMapping()
-  public Page<ProductDTO> findAll(Pageable pageable) {
-   return service.findAll(pageable);
+  public ResponseEntity<Page<ProductDTO>> findAll(Pageable pageable) {
+   Page<ProductDTO> dto = service.findAll(pageable);
+   return ResponseEntity.ok(dto);
   }
 
 
+    // @PostMapping
+    // public ProductDTO insert(@RequestBody ProductDTO dto) {
+        
+    //    return service.insert(dto);
+    // }
+  @PostMapping
+  public ResponseEntity<ProductDTO> insert(@RequestBody ProductDTO dto) {
+      
+     dto = service.insert(dto);
+     URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
+     .buildAndExpand(dto.getId()).toUri();
+
+     return ResponseEntity.created(uri).body(dto);
+  }
+  
 }
