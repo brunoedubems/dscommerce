c6f3dbcdf5cbcc19fb5f6e3ce9351a9d8ae62161
brunoedubems <bemds1@aluno.ifnmg.edu.br>
2025-07-12 19:49:21 -0300

    domain model User Role , seed

--- src/main/java/com/brunoedubems/dscommerce/entities/User.java
+++ src/main/java/com/brunoedubems/dscommerce/entities/User.java
@@ -2,13 +2,18 @@ package com.brunoedubems.dscommerce.entities;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
@@ -30,6 +35,14 @@ public class User {
 
   @OneToMany(mappedBy = "client")
   private List<Order> orders = new ArrayList<>();
+
+    @ManyToMany
+    @JoinTable(name = "tb_user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+
+
 
   public User() {
   }
@@ -95,6 +108,21 @@ public class User {
     return orders;
   }
 
+    public void addRole(Role role) {
+    	roles.add(role);
+    }
+    
+	public boolean hasRole(String roleName) {
+		for (Role role : roles) {
+			if (role.getAuthority().equals(roleName)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+
+  
   @Override
   public int hashCode() {
     final int prime = 31;
@@ -117,6 +145,10 @@ public class User {
     } else {
       return id.equals(other.id);
     }
+  }
+
+  public Set<Role> getRoles() {
+    return roles;
   }
 
 }
